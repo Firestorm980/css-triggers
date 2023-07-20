aed3ab5f33b52c5a2c1c7d1403ea9c50e5106252
Jon Christensen <jon.christensen@get10up.com>
2023-07-20 14:37:10 -0500

    Create config, remove manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,48 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import AstroPWA from '@vite-pwa/astro'
+
+const base = '/css-triggers'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://firestorm.github.io',
-	base: '/css-triggers',
+	base,
+	vite: {
+		define: {
+			__DATE__: `'${new Date().toISOString()}'`,
+		},
+	},
+	integrations: [
+		AstroPWA({
+			manifest: {
+				base,
+				name: 'CSS Triggers',
+				short_name: 'CSS Triggers',
+				theme_color: '#ffffff',
+				background_color: "#ffffff",
+		    	display: "standalone",
+				description: 'A reference for the render impact of mutating CSS properties.',
+				icons: [
+					{
+						src: `${base}/assets/images/favicons/android-chrome-192x192.png`,
+						sizes: "192x192",
+						type: "image/png"
+					},
+					{
+						src: `${base}/assets/images/favicons/android-chrome-512x512.png`,
+						sizes: "512x512",
+						type: "image/png"
+					}
+				],
+			},
+			workbox: {
+			  navigateFallback: `${base}/404`,
+			  globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+			},
+			devOptions: {
+			  enabled: true,
+			  navigateFallbackAllowlist: [/^\/css-triggers\/404$/],
+			},
+		})
+	]
 });
