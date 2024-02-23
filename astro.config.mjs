243b48f8dc229cca6020f621f9c46fc08d0d344d
Jon Christensen <jon.christensen@get10up.com>
2024-02-23 12:56:52 -0600

    PWA fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import AstroPWA from '@vite-pwa/astro'
 
-const base = '/css-triggers'
+const base = '/css-triggers/'
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,8 +14,8 @@ export default defineConfig({
 	},
 	integrations: [
 		AstroPWA({
+			base,
 			manifest: {
-				base,
 				name: 'CSS Triggers',
 				short_name: 'CSS Triggers',
 				theme_color: '#ffffff',
@@ -24,24 +24,24 @@ export default defineConfig({
 				description: 'A reference for the render impact of mutating CSS properties.',
 				icons: [
 					{
-						src: `${base}/assets/images/favicons/android-chrome-192x192.png`,
+						src: `assets/images/favicons/android-chrome-192x192.png`,
 						sizes: "192x192",
 						type: "image/png"
 					},
 					{
-						src: `${base}/assets/images/favicons/android-chrome-512x512.png`,
+						src: `assets/images/favicons/android-chrome-512x512.png`,
 						sizes: "512x512",
 						type: "image/png"
 					}
 				],
 			},
 			workbox: {
-			  navigateFallback: `${base}/404`,
+			  navigateFallback: `/`,
 			  globPatterns: ['**/*.{css,js,html,svg,png,ico}'],
 			},
 			devOptions: {
 			  enabled: true,
-			  navigateFallbackAllowlist: [/^\/css-triggers\/404$/],
+			  navigateFallbackAllowlist: [/^\/$/],
 			},
 		})
 	]
